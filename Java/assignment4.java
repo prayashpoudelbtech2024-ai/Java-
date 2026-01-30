d471a38e8eae92b6c2080f2f8ac2d3934e144398
prayashpoudelbtech2024-ai <prayash.poudel.btech2024@sitpune.edu.in>
2026-01-31 00:06:08 +0530

    Add files via upload

---
+++ Java/assignment4.java
@@ -0,0 +1,11 @@
+class ParamMethod {
+
+    static void add(int a, int b) {
+        System.out.println("Sum = " + (a + b));
+    }
+
+    public static void main(String[] args) {
+        add(10, 20);
+    }
+}
+
